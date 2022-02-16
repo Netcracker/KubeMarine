092fb614d06595d8ce2934e3941fa965e2cda729
Dmitry Yarovov <dmitrii.yarovov@netcracker.com>
2022-02-16 19:30:18 +0300

    audit

--- kubemarine/core/cluster.py
+++ kubemarine/core/cluster.py
@@ -342,7 +342,7 @@ class KubernetesCluster(Environment):
         utils.dump_file(self, output, "cluster_finalized.yaml")
         cluster_storage = utils.ClusterStorage.get_instance(self)
         cluster_storage.upload_file(self, output, "cluster_finalized.yaml")
-
+        cluster_storage.pack_file(self)
 
     def escape_jinja_characters_for_inventory(self, obj):
         if isinstance(obj, dict):
