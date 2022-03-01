0712545d00e741d588eea9593d048f0a6678039e
Dmitry Yarovov <dmitrii.yarovov@netcracker.com>
2022-03-01 14:05:10 +0300

    add new functional in audit task

--- kubemarine/procedures/install.py
+++ kubemarine/procedures/install.py
@@ -141,7 +141,8 @@ def system_prepare_policy(cluster):
             master.sudo("kubeadm init phase control-plane apiserver --config=/etc/kubernetes/audit-on-config.yaml")
             master.sudo("kubeadm init phase upload-config kubeadm --config=/etc/kubernetes/audit-on-config.yaml")
 
-    cluster.nodes['master'].get_first_member().call(utils.wait_command_successful, command="kubectl get pod -A")
+    for masters in cluster.nodes['master'].get_ordered_members_list():
+        masters.call(utils.wait_command_successful, command="kubectl get pod -A | grep 'kube-apiserver'")
 
 def system_prepare_dns_hostname(cluster):
     with RemoteExecutor(cluster):
