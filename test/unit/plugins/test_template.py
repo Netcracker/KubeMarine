9787f64ce83ea832bd7a20e156b3c0a298d389d1
ilzh0318 <ilia.zharkov@netcracker.com>
2022-04-14 12:22:32 +0300

    Mask templates in plugins breakage

--- test/unit/plugins/test_template.py
+++ test/unit/plugins/test_template.py
@@ -70,17 +70,19 @@ class TestTemplate(unittest.TestCase):
         test_cases = [
             {
                 "name": "One yaml template",
-                "create_files": ["./test_templates/test_template.yaml"],
+                "apply_files": ["test_template1.yaml"],
                 "source": "../test/unit/plugins/test_templates/test_template1.yaml",
                 "valid": True,
             },
             {
                 "name": "Wildcard path matching three yaml templates",
+                "apply_files": ["test_template1.yaml", "test_template2.yaml", "test_template3.yaml"],
                 "source": "../test/unit/plugins/test_templates/*.yaml",
                 "valid": True,
             },
             {
-                "name": "Directory wildcard path matching two yaml templates",
+                "name": "Directory wildcard path matching three yaml templates",
+                "apply_files": ["test_template1.yaml", "test_template2.yaml", "test_template3.yaml"],
                 "source": "../test/unit/plugins/test_templates/*",
                 "valid": True,
             },
@@ -105,16 +107,24 @@ class TestTemplate(unittest.TestCase):
                 # Create new test config with the source value
                 config = {
                     "source": tc["source"],
-                    'apply_required': False,
                 }
 
                 if tc["valid"]:
+                    for file in tc["apply_files"]:
+                        result = demo.create_nodegroup_result(cluster.nodes["master"])
+                        cluster.fake_shell.add(result, "sudo", [f"kubectl apply -f /etc/kubernetes/{file}"], usage_limit=1)
+
                     # If test case is valid just run the function
                     apply_template(cluster, config)
+
+                    for file in tc["apply_files"]:
+                        history = cluster.fake_shell.history_find("sudo", [f"kubectl apply -f /etc/kubernetes/{file}"])
+                        self.assertEqual(1, len(history))
+                        self.assertEqual(1, history[0]["used_times"])
                 else:
                     # If test case is not valid check for exception raise
                     self.assertRaises(
-                        Exception,
+                        ValueError,
                         apply_template,
                         cluster, config
                     )
