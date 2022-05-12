eabe4bdc192a961d7a8a052ede4dbd3b4196bbc2
Dmitry Yarovov <dmitrii.yarovov@netcracker.com>
2022-05-12 16:13:50 +0300

    Fix migrate procedure

--- kubemarine/procedures/migrate_cri.py
+++ kubemarine/procedures/migrate_cri.py
@@ -164,6 +164,9 @@ def _migrate_cri(cluster, node_group):
         kubeadm_flags = node["connection"].sudo(f"cat {kubeadm_flags_file}",
                                                 is_async=False).get_simple_out()
 
+        if kubeadm_flags.find('--network-plugin=cni') != -1:
+            kubeadm_flags = kubeadm_flags.replace('--network-plugin=cni', '')
+
         kubeadm_flags = edit_config(kubeadm_flags)
 
         node["connection"].put(io.StringIO(kubeadm_flags), kubeadm_flags_file, backup=True, sudo=True)
