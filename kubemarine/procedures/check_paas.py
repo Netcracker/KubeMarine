109beb24bef5b53ba0cddca7df0098cb24409726
NS <29598073+n549@users.noreply.github.com>
2022-05-25 16:54:18 +0300

    fix get_not_running_pods()

--- kubemarine/procedures/check_paas.py
+++ kubemarine/procedures/check_paas.py
@@ -451,7 +451,8 @@ def kubernetes_nodes_condition(cluster, condition_type):
 
 
 def get_not_running_pods(cluster):
-    get_pods_cmd = 'kubectl get pods -A --field-selector status.phase!=Running | awk \'{ print $1" "$2" "$4 }\''
+    # Completed pods should be excluded from the list as well
+    get_pods_cmd = 'kubectl get pods -A --field-selector status.phase!=Running | awk \'{ print $1" "$2" "$4 }\' | grep -vw Completed'
     result = cluster.nodes['control-plane'].get_any_member().sudo(get_pods_cmd)
     cluster.log.verbose(result)
     return list(result.values())[0].stdout.strip()
