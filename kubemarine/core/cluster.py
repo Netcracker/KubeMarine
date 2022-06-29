66497cb88e666804e8211e2d7783b53b3211a828
Aleksandr Arefev <39635005+alexarefev@users.noreply.github.com>
2022-06-29 19:43:43 +0300

    check_paas fix

--- kubemarine/core/cluster.py
+++ kubemarine/core/cluster.py
@@ -309,15 +309,17 @@ class KubernetesCluster(Environment):
             packages_list = []
             final_packages_list = []
             if isinstance(associated_packages, str):
-                packages_list.append(packages.get_package_name(os_family, associated_packages))
+                packages_list.append(packages.get_package_name(self.nodes['all'].get_nodes_os(), associated_packages))
+                pack = packages.get_package_name(os_family, associated_packages)
             elif isinstance(associated_packages, list):
                 associated_packages_clean = []
                 for package in associated_packages:
-                     associated_packages_clean.append(packages.get_package_name(os_family, package))
+                     associated_packages_clean.append(packages.get_package_name(self.nodes['all'].get_nodes_os(), package))
                 packages_list = packages_list + associated_packages_clean
             else:
                 raise Exception('Unsupported associated packages object type')
-
+            #self.log.debug("PAK: %s" % packages_list)
+            #self.log.debug("DETECT_PAK: %s" % detected_packages)
             for package in packages_list:
                 detected_package_versions = list(detected_packages[package].keys())
                 for version in detected_package_versions:
