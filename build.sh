9e1750e47b7dda115a56e426bd46d99f63ef2ab2
dmya0721 <dmitrii.yarovov@netcracker.com>
2022-10-25 12:46:19 +0300

    build single kubemarine

--- build.sh
+++ build.sh
@@ -16,4 +16,9 @@ for id in $DOCKER_NAMES; do
     docker tag "${NAME}" "$id"
 done
 
+docker build -t "${NAME}_binary" --build-arg BUILD_TYPE=binary --no-cache .
+CONTAINER_ID=$(docker create "${NAME}_binary")
+docker cp "$CONTAINER_ID:/opt/kubemarine/dist/kubemarine" kubemarine
+docker rm -v "${CONTAINER_ID}"
+
 chmod +x kubemarine
