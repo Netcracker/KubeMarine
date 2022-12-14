632593091e419177fff683676b049042a0d3b888
Aleksandr Arefev <39635005+alexarefev@users.noreply.github.com>
2022-12-14 16:26:41 +0300

    Patch

--- kubemarine/patches/__init__.py
+++ kubemarine/patches/__init__.py
@@ -22,7 +22,9 @@ The whole directory is automatically cleared and reset after new version of Kube
 from typing import List
 
 from kubemarine.core.patch import Patch
+from kubemarine.patches.p1_upgrade_containerdio_version import UpgrateContainerdioVersion
 
 patches: List[Patch] = [
+    UpgrateContainerdioVersion()
 ]
 """List of patches which can be executed strictly in the declared order"""
