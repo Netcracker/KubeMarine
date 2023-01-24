d0e484188719181776bfe669d5ad6b04033931c9
ilia1243 <8808144+ilia1243@users.noreply.github.com>
2023-01-24 14:55:33 +0300

    Fix verify_shell where target nodes are not explicitly provided

--- kubemarine/plugins/__init__.py
+++ kubemarine/plugins/__init__.py
@@ -562,8 +562,10 @@ def convert_shell(cluster, config):
 
 def verify_shell(cluster, config):
     out_vars = config.get('out_vars', [])
-    explicit_group = cluster.create_group_from_groups_nodes_names(config.get('groups', []), config.get('nodes', []))
-    if out_vars and explicit_group.nodes_amount() != 1:
+    groups = config.get('groups', [])
+    nodes = config.get('nodes', [])
+    explicit_group = cluster.create_group_from_groups_nodes_names(groups, nodes)
+    if out_vars and (groups or nodes) and explicit_group.nodes_amount() != 1:
         raise Exception('Shell output variables could be used for single-node groups, but multi-node group was found')
 
     in_vars = config.get('in_vars', [])
