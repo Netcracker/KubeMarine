eba8341c83582fc5496f76242063fc303968ffee
aaha0922 <127078005+aaha0922@users.noreply.github.com>
2023-03-13 11:59:56 +0530

    verify_python

--- kubemarine/plugins/__init__.py
+++ kubemarine/plugins/__init__.py
@@ -522,6 +522,11 @@ def apply_expect(cluster, config, plugin_name=None):
 # **** PYTHON ****
 
 def verify_python(cluster, step):
+    module_path, _ = utils.determine_resource_absolute_file(step['module'])
+    method_name = step['method']
+    method_arguments = step.get('arguments', {})
+    if method_name and module_path and method_arguments and module() != 1
+       raise Exception('Python output variables could be used for single-node groups, but multi-node group was found')
     # TODO: verify fields types and contents
     return
 
