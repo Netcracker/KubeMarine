6f7c74561237c5e9129a155a9c52f8b120f6c642
pranavcracker <113030526+pranavcracker@users.noreply.github.com>
2023-03-24 19:22:28 +0530

    Update thirdparties.py

--- kubemarine/thirdparties.py
+++ kubemarine/thirdparties.py
@@ -202,9 +202,9 @@ def install_thirdparty(filter_group: NodeGroup, destination: str) -> NodeGroupRe
             cluster.log.verbose('Unzip will be used for unpacking')
             remote_commands += ' && sudo unzip -o %s -d %s' % (destination, config['unpack'])
             
-            remote_commands += ' && sudo unzip -l %s | grep -v /$ | grep -vw -e Name -e "----" | awk \'NF > 3 { print $4 }\'| xargs -I FILE sudo chmod -R %s %s/FILE' \
+            remote_commands += ' && sudo unzip -qq -l %s | awk \'NF > 3 { print $4 }\'| xargs -I FILE sudo chmod -R %s %s/FILE' \
                    % (destination, config['mode'], config['unpack'])
-            remote_commands += ' && sudo unzip -l %s | grep -v /$ | grep -vw -e Name -e "----" | awk \'NF > 3 { print $4 }\'| xargs -I FILE sudo chown -R %s %s/FILE' \
+            remote_commands += ' && sudo unzip -qq -l %s | awk \'NF > 3 { print $4 }\'| xargs -I FILE sudo chown -R %s %s/FILE' \
                    % (destination, config['owner'], config['unpack'])
             remote_commands += ' && sudo ls -la %s' % (config['unpack'])
             
