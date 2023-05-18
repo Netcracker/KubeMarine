8a13a12f191b748024af2726cb496f485d3626a4
Nikhil A M <100134503+nikhil1697@users.noreply.github.com>
2023-05-18 18:13:35 +0530

    Update install.py

--- kubemarine/procedures/install.py
+++ kubemarine/procedures/install.py
@@ -509,7 +509,7 @@ def deploy_kubernetes_init(cluster: KubernetesCluster):
         kubernetes.join_other_control_planes
     ])
 
-    if false:
+    if 'worker' in cluster.nodes:
         cluster.nodes.get('worker').new_group(
             apply_filter=lambda node: 'control-plane' not in node['roles']) \
             .call(kubernetes.init_workers)
