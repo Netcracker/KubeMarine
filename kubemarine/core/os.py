e62bda0edad820277fbc3393fb5210fc3aa6598e
ilia1243 <8808144+ilia1243@users.noreply.github.com>
2023-05-23 17:36:20 +0300

    Implemented API to work with env variables

---
+++ kubemarine/core/os.py
@@ -0,0 +1,24 @@
+import os
+from typing import Iterator, Mapping
+
+
+class Environ(Mapping[str, str]):
+    """
+    Read-only view of os.environ.
+    """
+
+    def __getitem__(self, name: str) -> str:
+        # check presence of the variable and throw KeyError if necessary
+        return os.environ[name]
+
+    def __len__(self) -> int:
+        return len(os.environ)
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(os.environ)
+
+    __slots__ = []
+
+
+def environ() -> Environ:
+    return Environ()
