6fb0d580a664a7383ef37fc565dba5fc917f4c44
pranavcracker <113030526+pranavcracker@users.noreply.github.com>
2023-06-23 15:58:03 +0530

    added kubelet restart command

--- kubemarine/procedures/upgrade.py
+++ kubemarine/procedures/upgrade.py
@@ -148,6 +148,7 @@ def upgrade_containerd(cluster: KubernetesCluster):
                                         is_async=False).get_simple_out()
                 updated_kubeadm_flags = kubernetes._config_changer(kubeadm_flags, f"pause:{pause_version}",delemeter=":")
                 node['connection'].put(StringIO(updated_kubeadm_flags), kubeadm_flags_file, backup=True, sudo=True)
+                node['connection'].sudo("systemctl restart kubelet")
                 
 
             with RemoteExecutor(cluster) as exe:
