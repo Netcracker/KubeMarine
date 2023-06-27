1e6bd8e9e9d959d834bca013fdaf100a0a9f35a2
ilia1243 <8808144+ilia1243@users.noreply.github.com>
2023-06-27 11:15:34 +0300

    Fix breakage in procedure of adding workers

--- kubemarine/procedures/add_node.py
+++ kubemarine/procedures/add_node.py
@@ -39,7 +39,7 @@ def deploy_kubernetes_join(cluster: KubernetesCluster):
     cluster.nodes['control-plane'].get_new_nodes().call(kubernetes.join_new_control_plane)
 
     if "worker" in cluster.nodes:
-        cluster.nodes['worker'].exclude_group(cluster.nodes['control-plane']) \
+        cluster.nodes['worker'].get_new_nodes().exclude_group(cluster.nodes['control-plane']) \
             .call(kubernetes.init_workers)
 
     group.call_batch([
