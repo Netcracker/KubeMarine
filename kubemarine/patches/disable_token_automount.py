bcd16893a2214f3d5a268fca80baf8437a65d1e6
Pranav Patil <pranav.patil@netcracker.com>
2024-03-05 15:45:18 +0530

    improved plugin selection in patch

--- kubemarine/patches/disable_token_automount.py
+++ kubemarine/patches/disable_token_automount.py
@@ -1,10 +1,11 @@
 from textwrap import dedent
 from io import StringIO
-
+from typing import List
 from kubemarine.core.action import Action
 from kubemarine.core.patch import RegularPatch
 from kubemarine.core.resources import DynamicResources
-from kubemarine import kubernetes, plugins
+# from kubemarine import plugins
+from kubemarine.plugins import install 
 from kubemarine.plugins.manifest import Processor, EnrichmentFunction, Manifest, Identity
  
 class TheAction(Action):
@@ -13,8 +14,13 @@ class TheAction(Action):
     
     def run(self, res: DynamicResources) -> None:
         cluster = res.cluster() 
-        cluster.log.debug("As part of this patch, all the plugins will be redeployed")
-        plugins.install(cluster)      
+        def redeploy_oob_plugins( cluster, plugin_names: List[str]) -> None:
+            plugins = cluster.inventory["plugins"]
+            specific_plugins = {name: plugins[name] for name in plugin_names if name in plugins}
+            install(cluster, specific_plugins)
+        cluster.log.debug("As part of this patch, all the OOB plugins will be redeployed")
+        oob_plugins = ["calico", "nginx-ingress-controller", "kubernetes-dashboard", "local-path-provisioner" ]  # List of plugins to install
+        redeploy_oob_plugins(cluster, oob_plugins)     
 
 
 class DisableTokenAutomount(RegularPatch):
