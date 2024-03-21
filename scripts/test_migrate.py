690d9b7b6a43706bf11032cc1b07dc197b8478de
sergey kryazhev <sergey.kryazhev@netcracker.com>
2024-03-21 17:27:19 +0300

    tests

--- scripts/test_migrate.py
+++ scripts/test_migrate.py
@@ -13,15 +13,21 @@ def test_list_version():
     assert not list_versions("1","2")
 
 def test_get_patches_info():
-    assert get_patches_info("/tmp/kubemarine-linux-x86_64-v0.28.0").get("patches") # some patches
-    assert not get_patches_info("/tmp/kubemarine-linux-x86_64-v0.27.0").get("patches") # no patches
-    assert not get_patches_info("no file") 
-    assert not get_patches_info("/bin/ls").get("patches")
+    get_kubemarine_env("v0.28.0","bin")
+    get_kubemarine_env("v0.27.0","bin")
+assert (get_patches_info(
+    f"{tempfile.gettempdir()}/kubemarine-"
+    f"{'macos11' if platform.system().lower() == 'darwin' else 'linux'}-"
+    f"{platform.machine().lower()}-v0.28.0").get("patches")) #some patches
+assert not get_patches_info(
+    f"{tempfile.gettempdir()}/kubemarine-"
+    f"{'macos11' if platform.system().lower() == 'darwin' else 'linux'}-"
+    f"{platform.machine().lower()}-v0.27.0").get("patches") # no patches
+assert not get_patches_info("no file") 
+assert not get_patches_info("/bin/ls").get("patches")
     
 
 def test_get_kubemarine_env():
     assert get_kubemarine_env("v0.28.0","bin")
     assert not get_kubemarine_env("none","bin")
     assert not get_kubemarine_env("v0.28.0","None")
-
-    
