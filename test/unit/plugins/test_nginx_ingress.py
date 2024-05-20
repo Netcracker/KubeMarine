423a2484e88fb38f5d8a40727965ae9b8879789c
Pranav Patil <pranav.patil@netcracker.com>
2024-05-20 15:02:44 +0530

    small correction

--- test/unit/plugins/test_nginx_ingress.py
+++ test/unit/plugins/test_nginx_ingress.py
@@ -202,7 +202,7 @@ class ManifestEnrichment(_AbstractManifestEnrichmentTest):
                 # Check if nginx-ingress-controller version is v1.9.5 or v1.9.6 before counting webhook resources
                 nginx_version = self.get_obj(manifest, "DaemonSet_ingress-nginx-controller")\
                     ['spec']['template']['spec']['containers'][0]['image']
-                if "v1.9.5" in nginx_version or "v1.9.6" in nginx_version or "v1.10.0" in nginx_version:
+                if "v1.9.5" in nginx_version or "v1.9.6" in nginx_version or "v1.10.1" in nginx_version:
                     expected_num_resources = 9  # Adjust expected number for v1.9.5 and v1.9.6
 
                 for key in self.all_obj_keys(manifest):
