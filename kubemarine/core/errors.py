4a47b7fae5070fc2717f2ddd390818d6c79951b9
koryaga <5212888+koryaga@users.noreply.github.com>
2024-10-31 18:20:56 +0300

    Update errors.py

--- kubemarine/core/errors.py
+++ kubemarine/core/errors.py
@@ -91,7 +91,8 @@ def get_kme_dictionary() -> dict:
                     "Please, specify required 'sandbox_image' explicitly in procedure inventory."
         },
         'KME0014': {
-            "name": "The provided Helm chart URL {url} does not return {type} content in the {destination} file. Please validate the URL. If this is a private repository, ensure the correct authentication is provided."
+            "name": "The provided Helm chart URL {url} does not return {type} content in the {destination} file. "
+                    "Please validate the URL. If this is a private repository, ensure the correct authentication is provided."
         }
     }
 
