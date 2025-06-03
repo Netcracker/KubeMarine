b916f32a645068ccf337060b6fe60a389553f138
theboringstuff <39027092+theboringstuff@users.noreply.github.com>
2025-06-03 12:47:11 +0500

    initial

---
+++ kubemarine/patches/containerd_upgrade.py
@@ -0,0 +1,130 @@
+# Copyright 2021-2023 NetCracker Technology Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from textwrap import dedent
+
+from kubemarine import packages
+from kubemarine import kubernetes
+from kubemarine.core.action import Action
+from kubemarine.core.patch import RegularPatch
+from kubemarine.core.resources import DynamicResources
+from kubemarine.kubernetes import components
+
+
+class ContainerdUpgradeAction(Action):
+    def __init__(self) -> None:
+        super().__init__("Upgrade containerd")
+
+    def run(self, res: DynamicResources) -> None:
+        cluster = res.cluster()
+
+        # this patch is relevant only for ubuntu
+        os_family = cluster.get_os_family()
+        if os_family != "debian":
+            return
+        
+        # this is a containerd package which we expect to be present on nodes, including version
+        containerd_package = packages.get_association_packages(cluster, os_family, "containerd")
+
+        # find hosts where this expected package version is not present
+        hosts_to_packages = {host: containerd_package for host in cluster.nodes["all"].get_hosts()}
+        packages_map = packages.detect_installed_packages_version_hosts(cluster, hosts_to_packages)
+        bad_hosts = set()
+        for package, version_map in packages_map.items():
+            package_name = package.replace("*", "")
+            for version in version_map.keys():
+                if not version.startswith(package_name):
+                    for host in version_map[version]:
+                        bad_hosts.add(host)
+        
+        # on these bad hosts, install containerd version as specified in inventory, 
+        # but do it carefully with drain, kubelet stop/start, pods sandboxes removal, uncordon, wait for pods
+        group = cluster.make_group(bad_hosts)
+        log = cluster.log
+        for node in group.get_ordered_members_list():
+            log.debug(f"Containerd will be upgraded on node {node.get_node_name()}")
+
+            # drain the node, but only best-effort
+            log.debug(f"Draining node {node.get_node_name()}")
+            drain_cmd = kubernetes.prepare_drain_command(cluster, node.get_node_name())
+            node.sudo(drain_cmd, warn=True, hide=False, pty=True)
+
+            log.debug(f"Stopping kubelet on node: {node.get_node_name()}")
+            node.sudo("systemctl stop kubelet")
+
+            log.debug(f"Removing all containers on node: {node.get_node_name()}")
+            node.run("for pod in $(sudo crictl pods -q); do " 
+                            "sudo crictl inspectp $pod | " 
+                            "grep '\"network\": \"NODE\"' > /dev/null || " 
+                            "sudo crictl rmp -f $pod; " 
+                    "done", warn=True)
+            node.sudo("crictl rmp -fa", warn=True)
+
+            cri_packages = cluster.get_package_association_for_node(node.get_host(), 'containerd', 'package_name')
+            log.debug(f"Installing {cri_packages} on node: {node.get_node_name()}")
+            packages.install(node, include=cri_packages, pty=True)
+
+            log.debug(f"Starting kubelet and uncordon node {node.get_node_name()}")
+            node.sudo(f"systemctl start kubelet; sudo kubectl uncordon {node.get_node_name()}")
+
+            # we need to make sure that control-plane pods are OK after restart, before moving to next node
+            if "control-plane" in node.get_config()["roles"]:
+                log.debug(f"Waiting control-plane containers to be ready on node {node.get_node_name()}")
+                get_container_from_cri = "sudo crictl ps --name {component} -q"
+                get_container_from_pod = (
+                    "sudo kubectl get pods -n kube-system {component}-{node} "
+                    "-o 'jsonpath={{.status.containerStatuses[0].containerID}}{{\"\\n\"}}' "
+                    "| sed 's|.\\+://\\(.\\+\\)|\\1|'")
+
+                test_refreshed_container = (
+                    f"("
+                    f"CONTAINER=$({get_container_from_cri}); "
+                    f"if [ -z \"$CONTAINER\" ]; then "
+                    f"  echo \"container '{{component}}' is not created yet\" >&2 ; exit 1; "
+                    f"fi "
+                    f"&& "
+                    f"if [ \"$CONTAINER\" != \"$({get_container_from_pod})\" ]; "
+                    f"  then echo \"Pod '{{component}}-{{node}}' is not refreshed yet\" >&2; exit 1; "
+                    f"fi "
+                    f")")
+
+                commands = []
+                for component in components.CONTROL_PLANE_COMPONENTS:
+                    commands.append(test_refreshed_container.format(component=component, node=node.get_node_name()))
+
+                expect_config = cluster.inventory['globals']['expect']['pods']['kubernetes']
+                node.wait_commands_successful(commands,
+                                            timeout=expect_config['timeout'],
+                                            retries=expect_config['retries'],
+                                            sudo=False, pty=True)
+                components.wait_for_pods(node)
+
+
+class ContainerdUpgrade(RegularPatch):
+    def __init__(self) -> None:
+        super().__init__("containerd_upgrade")
+
+    @property
+    def action(self) -> Action:
+        return ContainerdUpgradeAction()
+
+    @property
+    def description(self) -> str:
+        return dedent(
+            f"""\
+            This patch upgrades containerd 1.6 to 1.7 on the cluster.
+            Attention: upgrade is performed node-by-node with node draining,
+            so it takes time and is disruptive for cluster/workloads.
+            """.rstrip()
+        )
