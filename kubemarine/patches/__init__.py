bba85f13567a9ac05ec4b3507519d0f8734a9620
theboringstuff <theboringstuff@users.noreply.github.com>
2025-07-11 12:38:55 +0000

    Delete all patches after release

--- kubemarine/patches/__init__.py
+++ kubemarine/patches/__init__.py
@@ -22,10 +22,8 @@ The whole directory is automatically cleared and reset after new version of Kube
 from typing import List
 
 from kubemarine.core.patch import Patch
-from kubemarine.patches.containerd_upgrade import ContainerdUpgrade
 
 patches: List[Patch] = [
-    ContainerdUpgrade(),
 ]
 """
 List of patches that is sorted according to the Patch.priority() before execution.
