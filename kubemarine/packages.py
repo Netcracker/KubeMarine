7671cd8bfd9cb6ec66f0cbc19b8b4e58acc2ae64
niam0522 <nikhil.am@netcracker.com>
2025-09-05 02:07:29 +0530

    test fix

--- kubemarine/packages.py
+++ kubemarine/packages.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import re
-from typing import List, Dict, Tuple, Optional, Union, Mapping, Set, Protocol
+from typing import List, Dict, Tuple, Optional, Union, Mapping, Protocol
 from io import StringIO
 
 from kubemarine import yum, apt, jinja
