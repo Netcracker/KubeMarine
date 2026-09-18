78045b4aaf1c8284d588cbbf04042cc09056b569
theboringstuff <39027092+theboringstuff@users.noreply.github.com>
2026-09-18 17:33:15 +0500

    test

--- test/unit/test_cri_enrichment.py
+++ test/unit/test_cri_enrichment.py
@@ -51,6 +51,19 @@ class TestContainerdCriEnrichment(unittest.TestCase):
                                              'config_path for "io.containerd.grpc.v1.cri" plugin '
                                              'in services.cri.containerdConfig is provided')
 
+    def test_fail_if_limitnofile_soft_less_than_hard(self):
+        # Fail, if registry.mirror and config_path are configured
+        inventory = demo.generate_inventory(**demo.ALLINONE)
+        inventory['services']['cri'] = {
+            'containerdLimitNOFILE': {
+                'soft': 100,
+                'hard': 1,
+            }
+        }
+        self.do_failed_enrichment(inventory, 'Invalid containerd configuration: ' \
+                                             'services.cri.containerdLimitNOFILE ' \
+                                             'soft=100 can not be larger than hard=1')
+
     def test_fail_if_registry_configs_tls_and_config_path_are_used(self):
         # Fail, if registry.configs.tls and config_path are configured
         inventory = demo.generate_inventory(**demo.ALLINONE)
